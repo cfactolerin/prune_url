e8b6105c11eb482dcf3f39411eac99a83c090626
Cris Factolerin <cris.factolerin@gmail.com>
2018-11-10 16:11:25 +0800

    Added logic for Stats API

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -1,8 +1,75 @@
 module Api
   module V1
     class StatsController < ActionController::API
-      def index
+      before_action :link
+
+      ERROR_CODE_NOT_FOUND = "Code not found".freeze
+
+      def show
+        browsers = {}
+        os = {}
+        countries = {}
+        cities = {}
+
+        @link.viewers.each do |viewer|
+          browsers[viewer.browser] = (browsers[viewer.browser] || 0) + 1
+          os[viewer.os] = (os[viewer.os] || 0) + 1
+          countries[viewer.country] = (countries[viewer.country] || 0) + 1
+          cities[viewer.city] = (cities[viewer.city] || 0) + 1
+        end
+
+        stats = Stats.new("Success", params[:code], {
+            total_view_count: @link.viewers.sum(:view_count),
+            unique_viewers: @link.viewers.size,
+            browsers: browsers,
+            os: os,
+            countries: countries,
+            cities: cities
+        })
+        render json: stats
       end
+
+      def viewers
+        stats = Stats.new("Success", params[:code], {
+            viewers: @link.viewers
+        })
+        render json: stats
+      end
+
+      def viewers_by_country
+        stats = Stats.new("Success", params[:code], {
+            viewers: @link.viewers.where(country: params[:country])
+        })
+        render json: stats
+      end
+
+      def viewers_by_browser
+        stats = Stats.new("Success", params[:code], {
+            viewers: @link.viewers.where(browser: params[:browser])
+        })
+        render json: stats
+      end
+
+      private
+
+      def link
+        @link = Link.includes(:viewers).find_by(code: params[:code])
+        if @link.nil?
+          render json: Stats.new(ERROR_CODE_NOT_FOUND, params[:code])
+        end
+      end
+
+      class Stats
+        attr_accessor :api_version, :message, :code, :data
+
+        def initialize(message, code, data=nil)
+          @api_version = "v1"
+          @message = message
+          @code = code
+          @data = data
+        end
+      end
+
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :links, only: [:index, :create] do
-    collection do
-      get 'visit/:code' => :redirect
+  resources :links, only: [:index, :create]
+
+  namespace :api do
+    namespace :v1 do
+      resources :stats, param: :code, only: [:show] do
+        member do
+          get :viewers
+          get :viewers_by_country
+          get :viewers_by_browser
+        end
+      end
     end
   end
 
